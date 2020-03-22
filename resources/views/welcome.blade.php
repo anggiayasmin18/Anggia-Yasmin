4f0d244a5b5ba494f0004e3859fc4a345ca4490e
Anggia Yasmin <anggiayasmin08@gmail.com>
2020-03-22 16:09:36 +0800

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Anggia
                 </div>
 
                 <div class="links">
