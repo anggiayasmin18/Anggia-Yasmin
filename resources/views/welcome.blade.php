6758bd13b9dbee186ebd2d34419ef9afa568106e
Kinaaa12345 <sakinahita.isnaini04@gmail.com>
2020-03-22 16:05:36 +0800

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sakinahita
                 </div>
 
                 <div class="links">
